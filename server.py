14232e892d25af8ff6b372557e9c4a5d84b8f39f
ofhatwa <tofhatwa@gmail.com>
2025-01-22 23:44:35 +0200

    Updating the server file with the code that test if the server is running

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from website import create_app
+from flask import Flask
+from diary_app import create_app
+
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=1000)
+    app.run(debug=True, port=5000)
