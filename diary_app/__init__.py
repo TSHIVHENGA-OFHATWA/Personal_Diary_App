f4b6916e1a4f04e9ad6b8a539699f42de026d232
ofhatwa <tofhatwa@gmail.com>
2025-01-25 13:15:33 +0200

    Updating the init file with login manager to load the user

--- diary_app/__init__.py
+++ diary_app/__init__.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 import secrets
 from flask_sqlalchemy import SQLAlchemy
-from os  import path
+from os import path
 from flask_login import LoginManager
 
 load_dotenv()
@@ -14,24 +14,24 @@ DB_NAME = "dairy.db"
 
 def get_secret_key():
     """
-    Retrieve or generate the application's SECRET_KEY 
+    Retrieve or generate the application's SECRET_KEY
     if it doesn't exist in .env.
 
-    The function first attempts to retrieve the SECRET_KEY from the .env file. 
-    If not found, it generates a new 32-byte random key, saves it to the .env 
+    The function first attempts to retrieve the SECRET_KEY from the .env file.
+    If not found, it generates a new 32-byte random key, saves it to the .env
     file for future use, and returns it.
 
     Returns:
         str: The application's SECRET_KEY.
     """
-    
+
     secret_key = os.getenv('SECRET_KEY')
     if not secret_key:
         secret_key = secrets.token_hex(32)  # Generate a secure random key
         with open('.env', 'a') as env_file:
             env_file.write(f"\nSECRET_KEY={secret_key}")
         print("SECRET_KEY has been generated and saved to .env")
-    
+
     return secret_key
 
 
@@ -39,7 +39,7 @@ def create_app():
     """
     Create and configure the Flask application instance.
 
-    This function sets up the Flask app, initializes the database connection, 
+    This function sets up the Flask app, initializes the database connection,
     registers Blueprints for modularity, and configures Flask-Login.
 
     Returns:
@@ -53,18 +53,17 @@ def create_app():
     from .controllers import controllers
     from .authentication import authentication
 
-    app.register_blueprint(controllers,url_prefix='/')
-    app.register_blueprint(authentication,url_prefix='/')
+    app.register_blueprint(controllers, url_prefix='/')
+    app.register_blueprint(authentication, url_prefix='/')
 
     from .db_models import User, Diary
-    
+
     with app.app_context():
-       conn.create_all()
+        conn.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'authentication.login'
     login_manager.init_app(app)
-
 
     @login_manager.user_loader
     def load_user(id):
@@ -78,7 +77,6 @@ def create_app():
             User: The user instance corresponding to the given ID.
         """
         return User.query.get(int(id))
-    
     return app
 
 
